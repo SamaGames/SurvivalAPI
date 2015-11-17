e2ef3948587894a302d5ba5263e79e2d58840bef
BlueSlime <jeremylevilain78@orange.fr>
2015-11-17 20:21:35 +0100

    Fixed percentage

--- src/main/java/net/samagames/survivalapi/game/WorldLoader.java
+++ src/main/java/net/samagames/survivalapi/game/WorldLoader.java
@@ -34,7 +34,7 @@ public class WorldLoader
 
         this.task = Bukkit.getScheduler().runTaskTimer(this.plugin, new Runnable()
         {
-            private int todo = (1200 * 1200) / 256;
+            private int todo = ((size + 200) * (size + 200)) / 256;
             private int x = -(size + 100);
             private int z = -(size + 100);
 
