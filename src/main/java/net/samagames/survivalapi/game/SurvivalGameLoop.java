396643cc645ab2c0608269e567a9816a3742c920
BlueSlime <jeremylevilain78@orange.fr>
2015-11-19 00:58:33 +0100

    Changed algo

--- src/main/java/net/samagames/survivalapi/game/SurvivalGameLoop.java
+++ src/main/java/net/samagames/survivalapi/game/SurvivalGameLoop.java
@@ -229,19 +229,19 @@ public class SurvivalGameLoop implements Runnable
         if (angle > 337.5 || angle < 22.5)
             return "⬆";
         else if (angle > 22.5 && angle < 67.5)
-            return "⬉";
+            return "⬈";
         else if (angle > 67.5 && angle < 112.5)
-            return "⬅";
+            return "➡";
         else if (angle > 112.5 && angle < 157.5)
-            return "⬋";
+            return "⬊";
         else if (angle > 157.5 && angle < 202.5)
             return "⬇";
         else if (angle > 202.5 && angle < 247.5)
-            return "⬊";
+            return "⬋";
         else if (angle > 247.5 && angle < 292.5)
             return "➡";
         else
-            return "⬈";
+            return "⬉";
     }
 
     private String toString(int minutes, int seconds)
