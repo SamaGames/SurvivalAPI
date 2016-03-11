c8c7b93d68f265baa3f8bf6da2ed6b48bfd6a1dc
Silvanosky <geekpower14@gmail.com>
2016-03-11 18:41:25 +0100

    Remove general generation Added generation around spawns

--- src/main/java/net/samagames/survivalapi/game/events/GameListener.java
+++ src/main/java/net/samagames/survivalapi/game/events/GameListener.java
@@ -240,7 +240,7 @@ public class GameListener implements Listener
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event)
     {
-        if (this.game.isPvPActivated() && event.getBlockPlaced().getY() > WorldLoader.getHighestNaturalBlockAt(event.getBlockPlaced().getX(), event.getBlockPlaced().getZ()) + 15)
+        if (this.game.isPvPActivated() && event.getBlockPlaced().getY() > event.getBlock().getWorld().getHighestBlockYAt(event.getBlockPlaced().getX(), event.getBlockPlaced().getZ()) + 15)
         {
             event.setCancelled(true);
             event.getPlayer().sendMessage(ChatColor.DARK_RED + "[" + ChatColor.RED + "Tours" + ChatColor.DARK_RED + "] " + ChatColor.RED + "Les Tours sont interdites !");
