c6a5822755726b04e6448c0414a8fc10cf540c10
frasse_l <frasse_l@epitech.eu>
2016-04-04 20:10:02 +0200

    Added Chicken & GapZap & HighwayToHell modules

---
+++ src/main/java/net/samagames/survivalapi/modules/gameplay/ChickenModule.java
@@ -0,0 +1,50 @@
+package net.samagames.survivalapi.modules.gameplay;
+
+import net.samagames.api.games.GamePlayer;
+import net.samagames.survivalapi.SurvivalAPI;
+import net.samagames.survivalapi.SurvivalPlugin;
+import net.samagames.survivalapi.game.SurvivalGame;
+import net.samagames.survivalapi.modules.AbstractSurvivalModule;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * ChickenModule class
+ *
+ * Copyright (c) for SamaGames
+ * All right reserved
+ */
+public class ChickenModule extends AbstractSurvivalModule
+{
+    /**
+     * Constructor
+     *
+     * @param plugin Parent plugin
+     * @param api API instance
+     * @param moduleConfiguration Module configuration
+     */
+    public ChickenModule(SurvivalPlugin plugin, SurvivalAPI api, Map<String, Object> moduleConfiguration)
+    {
+        super(plugin, api, moduleConfiguration);
+    }
+
+    /**
+     * Set everybody's life to 1.5 hearts and give notch apple
+     *
+     * @param game Game
+     */
+    @Override
+    public void onGameStart(SurvivalGame game)
+    {
+        for (GamePlayer player : (Collection<GamePlayer>) game.getInGamePlayers().values())
+        {
+            Player p = player.getPlayerIfOnline();
+            p.getInventory().addItem(new ItemStack(Material.GOLDEN_APPLE, 1, (short)1));
+            p.setHealth(3);
+        }
+    }
+}
