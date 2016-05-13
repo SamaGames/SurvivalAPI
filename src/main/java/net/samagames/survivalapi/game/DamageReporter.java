7b64f62cdef231630d0d79bf3c687dfc76b98067
frasse_l <frasse_l@epitech.eu>
2016-05-13 20:24:42 +0200

    Rounded damages

--- src/main/java/net/samagames/survivalapi/game/DamageReporter.java
+++ src/main/java/net/samagames/survivalapi/game/DamageReporter.java
@@ -46,7 +46,7 @@ public class DamageReporter
         for (double damages : this.playerDamages.values())
             total += damages;
 
-        return total;
+        return (double)((long)(total * 10)) / 10D;
     }
 
     public Map<EntityType, Double> getEntityDamages()
