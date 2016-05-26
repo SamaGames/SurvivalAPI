62e2c03d646928c845200199617ba0f116c958ad
frasse_l <frasse_l@epitech.eu>
2016-05-26 14:07:27 +0200

    Try to fix TAM module

--- src/main/java/net/samagames/survivalapi/modules/combat/ThreeArrowModule.java
+++ src/main/java/net/samagames/survivalapi/modules/combat/ThreeArrowModule.java
@@ -1,9 +1,12 @@
 package net.samagames.survivalapi.modules.combat;
 
+import net.minecraft.server.v1_9_R2.EntityTippedArrow;
 import net.samagames.survivalapi.SurvivalAPI;
 import net.samagames.survivalapi.SurvivalPlugin;
 import net.samagames.survivalapi.modules.AbstractSurvivalModule;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftLivingEntity;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -45,6 +48,13 @@ public class ThreeArrowModule extends AbstractSurvivalModule
             return;
 
         for(int i = 0; i < 2; i++)
-            Bukkit.getScheduler().runTaskLater(this.plugin, () -> event.getEntity().getShooter().launchProjectile(Arrow.class, event.getEntity().getVelocity()).setMetadata("TAM", new FixedMetadataValue(this.plugin, true)), 5L * (i + 1));
+            Bukkit.getScheduler().runTaskLater(this.plugin, () ->
+            {
+                EntityTippedArrow entityTippedArrow = new EntityTippedArrow(((CraftWorld)event.getEntity().getWorld()).getHandle(), ((CraftLivingEntity)event.getEntity().getShooter()).getHandle());
+                entityTippedArrow.a(((CraftLivingEntity)event.getEntity().getShooter()).getHandle(), ((CraftLivingEntity)event.getEntity().getShooter()).getHandle().pitch, ((CraftLivingEntity)event.getEntity().getShooter()).getHandle().yaw, 0.0F, 3.0F, 1.0F);
+                entityTippedArrow.getBukkitEntity().setMetadata("TAM", new FixedMetadataValue(this.plugin, true));
+                entityTippedArrow.getBukkitEntity().setVelocity(event.getEntity().getVelocity());
+                ((CraftWorld)event.getEntity().getWorld()).getHandle().addEntity(entityTippedArrow);
+            }, 5L * (i + 1));
     }
 }
